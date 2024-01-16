6a80d122600ea0c53f6676789433b4394fcede80
hannahhannah20 <cc221009@fhstp.ac.at>
2024-01-16 13:36:44 +0100

    update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.cc221009.ccl3_leafminder"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
