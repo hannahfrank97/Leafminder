49edbf41c3bbe26c9d35587717f0a57ccf1787c6
cc221017 <andrea.windisch@gmx.com>
2024-01-17 16:41:44 +0100

    Theme Styling

--- app/src/main/java/com/cc221009/ccl3_leafminder/ui/theme/Color.kt
+++ app/src/main/java/com/cc221009/ccl3_leafminder/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.cc221009.ccl3_leafminder.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val greenDark = Color(0xFFC0C594)
+val greenLight = Color(0xFFEFEED4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val yelloDark = Color(0xFFFADC75)
+val yellowLight = Color(0xFFFFF1C1)
+
+val blueDark = Color(0xFF58A0D5)
+val blueLight = Color(0xFFDAE8F5)
+
+val blackgreen = Color(0xFF13420B)
+val blackgreen50 = Color(0x803C4B34)
+val white50 = Color(0x80FFFFFF)
+val white = Color(0xFFFFFFFF)
+
+val red = Color(0xFF993535)
+
+
--- app/src/main/java/com/cc221009/ccl3_leafminder/ui/theme/Theme.kt
+++ app/src/main/java/com/cc221009/ccl3_leafminder/ui/theme/Theme.kt
@@ -10,31 +10,60 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = blackgreen,
+    onPrimary = white,
+    primaryContainer = greenDark,
+    onPrimaryContainer = blackgreen,
+    inversePrimary = greenDark,
+    secondary = blueDark,
+    onSecondary = white,
+    secondaryContainer = blueLight,
+    onSecondaryContainer = blackgreen,
+    tertiary = yelloDark,
+    onTertiary = blackgreen,
+    tertiaryContainer = yellowLight,
+    onTertiaryContainer = blackgreen,
+    background = white,
+    onBackground = blackgreen,
+    surface = greenLight,
+    onSurface = blackgreen,
+    surfaceVariant = yellowLight,
+    onSurfaceVariant = blackgreen,
+    surfaceTint = blueLight,
+    error = red,
+    onError = white,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = blackgreen,
+    onPrimary = white,
+    primaryContainer = greenDark,
+    onPrimaryContainer = blackgreen,
+    inversePrimary = greenDark,
+    secondary = blueDark,
+    onSecondary = white,
+    secondaryContainer = blueLight,
+    onSecondaryContainer = blackgreen,
+    tertiary = yelloDark,
+    onTertiary = blackgreen,
+    tertiaryContainer = yellowLight,
+    onTertiaryContainer = blackgreen,
+    background = white,
+    onBackground = blackgreen,
+    surface = greenLight,
+    onSurface = blackgreen,
+    surfaceVariant = yellowLight,
+    onSurfaceVariant = blackgreen,
+    surfaceTint = blueLight,
+    error = red,
+    onError = white,
 )
 
 @Composable
