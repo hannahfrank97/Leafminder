31d62ada2136d167db74ff4542c428c3c9ac27e9
hannahhannah20 <cc221009@fhstp.ac.at>
2024-01-23 18:38:59 +0100

    CREATING new column in room database

--- app/src/main/java/com/cc221009/ccl3_leafminder/data/model/Plant.kt
+++ app/src/main/java/com/cc221009/ccl3_leafminder/data/model/Plant.kt
@@ -13,5 +13,6 @@ data class Plant(
     val wateringDate: String,
     val wateringFrequency: String,
     val imagePath: String,
+    val apiId: Int,
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
 )
